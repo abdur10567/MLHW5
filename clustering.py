3596657d81896bb2c286a03681e97290b31f833d
Abdur Rahman <abdur.r10567@gmail.com>
2022-12-04 01:54:14 -0800

    Finished clustering.py

--- clustering.py
+++ clustering.py
@@ -1,7 +1,7 @@
 #-------------------------------------------------------------------------
 # AUTHOR: Abdur Rahman
 # FILENAME: clustering.py
-# SPECIFICATION: description of the program
+# SPECIFICATION:
 # FOR: CS 4210- Assignment #5
 # TIME SPENT: how long it took you to complete the assignment
 #-----------------------------------------------------------*/
@@ -18,27 +18,51 @@ from sklearn import metrics
 df = pd.read_csv('training_data.csv', sep=',', header=None) #reading the data by using Pandas library
 
 #assign your training data to X_training feature matrix
+X_training = df.iloc[:]
 
 #run kmeans testing different k values from 2 until 20 clusters
      #Use:  kmeans = KMeans(n_clusters=k, random_state=0)
      #      kmeans.fit(X_training)
      #--> add your Python code
 
+maxSilhouetteCoef = 0
+kForMaxCoef = 2
+allCoefs = []
+for i in range(2,21):
+    kmeans = KMeans(n_clusters=i, random_state=0)
+    kmeans.fit((X_training))
 
-     #for each k, calculate the silhouette_coefficient by using: silhouette_score(X_training, kmeans.labels_)
-     #find which k maximizes the silhouette_coefficient
-     #--> add your Python code here
+    # for each k, calculate the silhouette_coefficient by using: silhouette_score(X_training, kmeans.labels_)
+    # find which k maximizes the silhouette_coefficient
+    # --> add your Python code here
+    currentSilhouetteCoef = silhouette_score(X_training,kmeans.labels_)
+    allCoefs.append((i,currentSilhouetteCoef))
+    if currentSilhouetteCoef > maxSilhouetteCoef:
+        maxSilhouetteCoef = currentSilhouetteCoef
+        kForMaxCoef = i
+
+
+
+print(maxSilhouetteCoef)
+print(kForMaxCoef)
 
 #plot the value of the silhouette_coefficient for each k value of kmeans so that we can see the best k
 #--> add your Python code here
+plt.scatter(*zip(*allCoefs))
+plt.ylabel("Silhouette Coefficient")
+plt.xlabel("k value")
+plt.show()
 
 #reading the test data (clusters) by using Pandas library
 #--> add your Python code here
+df_test = pd.read_csv('testing_data.csv', sep=',', header=None)
 
 #assign your data labels to vector labels (you might need to reshape the row vector to a column vector)
 # do this: np.array(df.values).reshape(1,<number of samples>)[0]
 #--> add your Python code here
+reshaped = np.array(df_test.values).reshape(1,len(df_test.index))[0]
 
 #Calculate and print the Homogeneity of this kmeans clustering
-print("K-Means Homogeneity Score = " + metrics.homogeneity_score(labels, kmeans.labels_).__str__())
+#print("K-Means Homogeneity Score = " + metrics.homogeneity_score(labels, kmeans.labels_).__str__())
 #--> add your Python code here
+print("K-Means Homogeneity Score = " + metrics.homogeneity_score(reshaped, kmeans.labels_).__str__())
